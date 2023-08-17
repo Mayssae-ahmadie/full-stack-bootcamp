a87a1df85fbdc599201859fc91db36e79377e21a
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-08-17 12:27:21 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,8 +1,10 @@
 console.log('I am linked now!');
 
+//Step 2//
 let name = "Mayssaê";
 alert(name);
 
+//Step 3//
 let age = 30;
 const lastName = "El Ahmadié";
 var DOB = "15-11-1992"
@@ -13,3 +15,27 @@ console.log(DOB);
 
 //The const value is constant. An error will occur if we assign a new value to the lastName//
 
+//Step 4//
+console.log(typeof ("This is a text"));
+console.log(typeof (true));
+console.log(typeof (false));
+console.log(typeof (1215));
+console.log(typeof ("999"));
+console.log(typeof ({}));
+console.log(typeof ([]));
+console.log(typeof (null));
+console.log(typeof (undefined));
+
+let v1 = 1234;
+let v2 = "Welcome to Javsascript";
+let v3 = {};
+let v4 = [1, 2, 3, 4, 5];
+let v5 = true
+let v6 = null;
+
+console.log(v1 + " " + typeof (v1));
+console.log(v2 + " " + typeof (v2));
+console.log(v3 + " " + typeof (v3));
+console.log(v4 + " " + typeof (v4));
+console.log(v5 + " " + typeof (v5));
+console.log(v6 + " " + typeof (v6));
