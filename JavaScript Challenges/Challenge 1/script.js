58dc431775ee3e1470df0fc62a620723209a8ff0
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-08-17 11:49:54 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -2,3 +2,14 @@ console.log('I am linked now!');
 
 let name = "Mayssaê";
 alert(name);
+
+let age = 30;
+const lastName = "El Ahmadié";
+var DOB = "15-11-1992"
+
+console.log(age);
+console.log(lastName);
+console.log(DOB);
+
+//The const value is constant. An error will occur if we assign a new value to the lastName//
+
