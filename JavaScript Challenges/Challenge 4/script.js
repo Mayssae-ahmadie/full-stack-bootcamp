0b5c83fe25fad33ee02f97899b1058e6785e36b1
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-08-22 15:56:44 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -2,6 +2,7 @@
 // Array Creation //
 
 let fiveNumbers = [5, 7, 13, 15, 19];
+console.log(fiveNumbers);
 
 // Object Creation //
 const personalInfo = {
@@ -11,3 +12,25 @@ const personalInfo = {
     location: "Sawfar",
     nationality: "Lebanese",
 };
+console.log(personalInfo);
+
+// Step 2 //
+// New Array with Spread //
+
+let nextThreeNumbers = [22, 24, 28];
+console.log(nextThreeNumbers);
+
+const allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+console.log(allNumbers);
+
+// New Object with Spread //
+const additionalInfo = {
+    occupation: "Dentist",
+    hobby: "Hiking",
+    education: "Doctor of dental surgery, USJ",
+};
+console.log(additionalInfo);
+
+const fullInfo = { ...personalInfo, ...additionalInfo };
+console.log(fullInfo);
+
