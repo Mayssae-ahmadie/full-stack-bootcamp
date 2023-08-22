e34cfd97913e29fbf44011903d8e8b12c3d9c608
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-08-22 15:02:32 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,13 @@
+// Step 1 //
+// Array Creation //
+
+let fiveNumbers = [5, 7, 13, 15, 19];
+
+// Object Creation //
+const personalInfo = {
+    name: "Mayssaê",
+    age: 30,
+    height: 1.55,
+    location: "Sawfar",
+    nationality: "Lebanese",
+};
