f26b4c51679009e8812fa8b86de0d6d356fd50f5
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-08-24 16:41:58 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -1,3 +1,4 @@
+// Step 1 //
 const newButton = document.createElement('button');
 newButton.textContent = 'Click Here';
 document.body.appendChild(newButton);
@@ -14,3 +15,35 @@ newButton.addEventListener("click", function () {
     newButton.style.color = 'white';
     newButton.textContent = 'Clicked' + Math.random() * 1000 + 1;
 });
+
+// Step 2 //
+const newHeading1 = document.createElement('h1');
+newHeading1.textContent = 'Hello';
+document.body.appendChild(newHeading1);
+
+newHeading1.style.border = '10px dotted green';
+
+// Function to handle keydown events
+function handleKeyDown(event) {
+    switch (event.key) {
+        case 'A':
+            newHeading1.style.backgroundColor = 'red';
+            break;
+        case 'S':
+            newHeading1.style.marginLeft = '10px';
+            break;
+        case 'D':
+            const paragraph = document.createElement('p');
+            paragraph.innerText = 'Key D was pressed!';
+            document.body.appendChild(paragraph);
+            break;
+        case 'W':
+            newHeading1.style.display = newHeading1.style.display === 'none' ? 'block' : 'none';
+            break;
+        case ' ':
+            newHeading1.style.fontSize = '40px';
+            break;
+    }
+}
+
+document.addEventListener('keydown', handleKeyDown);
