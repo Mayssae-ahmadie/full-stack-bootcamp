171a5b041791c4d5c8ecedb60d16834e806418b4
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-08-25 22:40:04 +0300

    Challenge 6 - Done with step 2 and 3

--- JavaScript Challenges/Challenge 6/script.js
+++ JavaScript Challenges/Challenge 6/script.js
@@ -33,22 +33,74 @@ const delayedPromise = new Promise((resolve, reject) => {
     }, 2000);
 });
 
-delayedPromise.then((message) => {
-    console.log(message);
-})
-
 delayedPromise
-    .then((message) => {
+    .then(message => {
         alert(message);
-
-        return 'Chained message!';
+        console.log("Chained message!");
     })
-    .then((chainedMessage) => {
-        console.log(chainedMessage);
-    })
-    .catch((error) => {
+    .catch(error => {
         console.error(error);
     });
+
+const networkRequest = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const shouldResolve = Math.random() >= 0.3;
+        if (shouldResolve) {
+            resolve("Data fetched successfully!");
+        } else {
+            reject("Network Error!");
+        }
+    }, 3000);
+});
+
+async function fetchData() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1%22");
+        if (!response.ok) {
+            throw new Error(`Fetch, failed, with status ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Fetched data:", data);
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+    }
+}
+
+networkRequest
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+fetchData
+
+
+const fetchAndLogPost = async postId => {
+    try {
+        const response = await fetch(
+            ` https://jsonplaceholder.typicode.com/posts/${postId}`
+        );
+        if (!response.ok) {
+            throw new Error(`Fetch, failed with status ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(`Fetched, post, ${postId}, data, data)`);
+    } catch (error) {
+        console.error(`Error, fetching, post, ${postId}, data, error.message`);
+    }
+};
+
+const postIds = [4, 5, 6, 7, 8];
+
+async function fetchPostsSequentially() {
+    for (const postId of postIds) {
+        await fetchAndLogPost(postId);
+    }
+}
+
+fetchPostsSequentially();
+
 
 
 
